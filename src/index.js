a570e87038d9a60c16339978d89796298af8f317
volodymyr <vova@dda.digital>
2020-02-24 14:54:04 +0200

    feat: add solutions task typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if(array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(Math, array);
+    }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if(array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(Math, array);
+    }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce((acc, cur) => acc + cur) / array.length;
+    }
+};
